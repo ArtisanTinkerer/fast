73e53504171566fff6c94572ca95eabd901e724e
Mick Byrne <mick.byrne@splend.com>
2024-11-22 21:26:19 +0000

    readme

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 @app.get("/")
 async def read_root():
@@ -9,3 +18,25 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name:ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
