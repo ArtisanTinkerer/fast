260da40ef893f5caf01ea708259f90cbc6e380a2
Mick Byrne <mick.byrne@splend.com>
2024-11-22 21:57:00 +0000

    Query param types.

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Working through:
 
 [Official Tutorial](https://fastapi.tiangolo.com/tutorial/first-steps/#check-it)
 
+
+[Currently here](https://fastapi.tiangolo.com/tutorial/query-params/#query-parameter-type-conversion)
+
 ## Docs
 
 [API Documentation](http://127.0.0.1:8000/docs)
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from enum import Enum
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -15,6 +17,7 @@ class ModelName(str, Enum):
 async def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
@@ -24,12 +27,14 @@ async def read_item(item_id: int):
 async def read_user_me():
     return {"user_id": "the current user"}
 
+
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
+
 @app.get("/models/{model_name}")
-async def get_model(model_name:ModelName):
+async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
@@ -38,5 +43,24 @@ async def get_model(model_name:ModelName):
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
+# query params if they are not part of the path parameters
+# query params can be optional and have defaults
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+# query parameter type conversion
+
+
+
+
+#so I can debug
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
